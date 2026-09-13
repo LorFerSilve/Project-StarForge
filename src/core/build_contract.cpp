488480d26c1966c2a05ec35bb859266a1b12bcae
LorFerSilve <silvetnefernandezlorenzo@gmail.com>
2026-09-13 13:18:06 +0200

    TA-16: add sf_core bootstrap implementation

---
+++ src/core/build_contract.cpp
@@ -0,0 +1,9 @@
+#include "starforge/core/build_contract.hpp"
+
+namespace starforge::core {
+
+std::string_view build_contract_name() noexcept {
+    return kImplementationBaseline;
+}
+
+}  // namespace starforge::core

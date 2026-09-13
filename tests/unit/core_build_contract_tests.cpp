ebba07f5bd346e3864797de2aa2471febaf0c603
LorFerSilve <silvetnefernandezlorenzo@gmail.com>
2026-09-13 13:18:36 +0200

    TA-16: add real bootstrap unit tests

---
+++ tests/unit/core_build_contract_tests.cpp
@@ -0,0 +1,20 @@
+#include "starforge/core/build_contract.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Core.BuildContract.TA16BaselineIsLocked", "[unit]") {
+    REQUIRE(starforge::core::build_contract_name() == "TA16-V1");
+}
+
+TEST_CASE("Core.BuildContract.AuthoritativeTickRateIs60Hz", "[unit]") {
+    REQUIRE(starforge::core::kAuthoritativeSimulationHz == 60U);
+}
+
+TEST_CASE("Core.Language.Cpp23BaselineIsActive", "[unit]") {
+    STATIC_REQUIRE(starforge::core::kRequiredCppStandard == 23U);
+#if defined(_MSVC_LANG)
+    STATIC_REQUIRE(_MSVC_LANG >= 202302L);
+#else
+    STATIC_REQUIRE(__cplusplus >= 202302L);
+#endif
+}
